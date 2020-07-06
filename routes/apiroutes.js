752443c363d4b1196375ffa2b603198e534008b8
Cranekick50 <michael.srithapin@gmail.com>
2020-07-05 21:07:29 -0500

    added apiroutes commands

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -14,12 +14,14 @@ router.get("/notes", function (req, res){
 // data: note,
 // method: "POST",
 router.post("/notes", function (req, res){
-//var note = req.body
+    var note = req.body;
+    repository.push(note);
     res.json();
 })
 // url: "api/notes/" + id,
 // method: "DELETE",
 router.delete("/notes:id", function (req, res){
-//var id = req.params.id
+    var id = req.params.id;
+    repository.deletenote(id);
     res.json();
 })
